b0f8ce38162cbdd4dbe29eb658be502fba694dce
Sinem Çalışkan <sinemcalskan.99@outlook.com>
2021-08-04 17:57:15 +0300

    test

--- app.js
+++ app.js
@@ -3,29 +3,43 @@ const exphbs = require("express-handlebars");
 const Handlebars = require('handlebars')
 const expressHandlebars = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-const fileUpload = require('express-fileupload')
-
 const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload')
+const moment = require('moment')
 
 
-mongoose.connect("mongodb://127.0.0.1/BProject_db", {
+
+
+mongoose.connect('mongodb://127.0.0.1/BProject_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 app.use(fileUpload())
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
+const hbs = exphbs.create({
+  helpers: {
+    generateDate : (date, format) => {
+      return moment(date).format(format)
+    }
+  }
+})
 
-app.engine("handlebars", expressHandlebars({
-  handlebars: allowInsecurePrototypeAccess(Handlebars)
-}), exphbs());
-app.set("view engine", "handlebars");
+app.engine('handlebars', expressHandlebars({
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+  helpers:{
+    generateDate:(date, format)=>{
+        return moment(date).format(format);
+    }
+}
+}));
+app.set('view engine', 'handlebars')
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
