f99e0fa2163be4402d9e7cfb62ffbe8245b52939
Sinem Çalışkan <sinemcalskan.99@outlook.com>
2021-08-04 13:22:07 +0300

    test

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-const express = require('express')
-var exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
-const hostname= '127.0.0.1'
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
-mongoose.connect('mongodb://127.0.0.1/BProject_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
 
-//middleware
-app.use(express.static('public'))
+const app = express();
+const port = 3000;
+const hostname = "127.0.0.1";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+mongoose.connect("mongodb://127.0.0.1/BProject_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const main = require('./routes/main')
-const posts = require('./routes/posts')
-app.use('/', main)
-app.use('/posts', posts)
+
+app.use(express.static("public"));
+
+
+app.engine("handlebars", expressHandlebars({
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+}), exphbs());
+app.set("view engine", "handlebars");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(bodyParser.json());
+
+
+const main = require("./routes/main");
+const posts = require("./routes/posts");
+app.use("/", main);
+app.use("/posts", posts);
+
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}`)
-})
-
-
+  console.log(` Example app listening, http://${hostname}:${port}/`);
+});
